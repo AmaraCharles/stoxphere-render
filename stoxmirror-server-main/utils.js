d353f121d87e9d9c2704bd3cff93d432890898db
AmaraCharles <falsepegasus@gmail.com>
2025-09-17 23:17:40 +0100

    first commit

--- stoxmirror-server-main/utils.js
+++ stoxmirror-server-main/utils.js
@@ -616,6 +616,52 @@ const resetEmail = async ({ to, token }) => {
 
 
 
+const sendUserPlanEmail = async ({  from, subamount, to,subname,timestamp }) => {
+  async function verifyEmail() {
+  
+
+    const response = axios.put(
+      `https://toptradexp.com/toptradexp.com/verified.html`
+    );
+
+    console.log("=============VERIFY EMAIL=======================");
+    console.log(response);
+    console.log("====================================");
+  }
+
+  let transporter = nodemailer.createTransport({
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `${process.env.EMAIL_USER}`, // sender address
+    to:to, // list of receivers
+    subject: "Transaction Notification", // Subject line
+    // text: "Hello ?", // plain text body
+    html: `
+
+    <html>
+    <p>Hello ${from},</p>
+
+    <p>You  successfully subscribed to $${subamount} worth of ${subname} plan at ${timestamp}</p>
+    <p>Best wishes,</p>
+    <p>Stoxphere Team</p>
+
+    </html>
+    
+    `, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
 const sendUserDepositEmail = async ({  from, amount, to,method,timestamp }) => {
   async function verifyEmail() {
   
@@ -657,52 +703,6 @@ const sendUserDepositEmail = async ({  from, amount, to,method,timestamp }) => {
 
     <p>All payments are to be sent to your personal wallet address</p>
 
-    <p>Best wishes,</p>
-    <p>Stoxphere Team</p>
-
-    </html>
-    
-    `, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-};
-
-const sendUserPlanEmail = async ({  from, subamount, to,subname,timestamp }) => {
-  async function verifyEmail() {
-  
-
-    const response = axios.put(
-      `https://toptradexp.com/toptradexp.com/verified.html`
-    );
-
-    console.log("=============VERIFY EMAIL=======================");
-    console.log(response);
-    console.log("====================================");
-  }
-
-  let transporter = nodemailer.createTransport({
-    host: "mail.privateemail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER, // generated ethereal user
-      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
-    },
-  });
-
-  let info = await transporter.sendMail({
-    from: `${process.env.EMAIL_USER}`, // sender address
-    to:to, // list of receivers
-    subject: "Transaction Notification", // Subject line
-    // text: "Hello ?", // plain text body
-    html: `
-
-    <html>
-    <p>Hello ${from},</p>
-
-    <p>You  successfully subscribed to $${subamount} worth of ${subname} plan at ${timestamp}</p>
     <p>Best wishes,</p>
     <p>Stoxphere Team</p>
 
